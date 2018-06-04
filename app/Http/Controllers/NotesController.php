4794cc60d2a889c67b6f367ffcf6855d55309f43
lucasdcrk <lordapofr@gmail.com>
2018-06-04 18:26:32 +0200

    Corrigé notes sur != 20

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -54,12 +54,7 @@ class NotesController extends Controller
 
                             if(is_numeric($note->valeur))
                             {
-                                if($note->noteSur == 10) {
-                                    $matiere->notes[] = $note * 2;
-                                }
-                                else {
-                                    $matiere->notes[] = $note;
-                                }
+                                $matiere->notes[] = (20 / $note->noteSur) * $note;
                                 $matiere->totalNotes = $matiere->totalNotes + $note->valeur * $note->coef;
                                 $matiere->totalCoefs = $matiere->totalCoefs + $note->coef;
                             }
